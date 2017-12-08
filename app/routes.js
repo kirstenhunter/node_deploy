8a5dc0c1d1ddfd8917c3e1b0fe4729649388c5ff
Kirsten Hunter <khunter@MacBook-Pro.local>
2017-12-07 16:50:07 -0800

    Fixing up some paths

--- app/routes.js
+++ app/routes.js
@@ -24,7 +24,7 @@ var Game = require('./models/game');
         // frontend routes =========================================================
         // route to handle all angular requests
         app.get('*', function(req, res) {
-            res.sendfile(__dirname + '/gametable/public/index.html'); // load our public/index.html file
+            res.sendfile(__dirname + '../gametable/public/index.html'); // load our public/index.html file
         });
 
     };
