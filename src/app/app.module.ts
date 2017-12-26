30c9af3206796e4d3fce706542e13d5d1f085ff5
Kirsten Hunter <synedra@gmail.com>
2017-12-25 23:32:37 -0800

    Adding the elastic beanstalk config to test integration with circleci

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MomentModule } from 'angular2-moment';
 
 import { Games } from './view-games.component'
 
@@ -12,7 +13,8 @@ import { Games } from './view-games.component'
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MomentModule
   ],
   providers: [],
   bootstrap: [Games]
