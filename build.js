f7dd009048a523c99bef2f6fdc54ad313d6dbf3e
Kirsten Hunter <synedra@gmail.com>
2018-01-12 12:29:39 -0800

    Adding a checkout for build

--- build.js
+++ build.js
@@ -7,7 +7,7 @@
 let ng_cmd = "ng build"
 
 if (process.env.NG_CMD && process.env.NG_CMD=="nobuild") {
-    process.exit(1);
+    process.exit(0);
 }
 if (process.env.NG_CMD) {
     ng_cmd = "ng build --environment " + process.env.NG_CMD 
